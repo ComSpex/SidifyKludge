2dc6bcfff89107e8285c5f4b6fcbde3c805c0c63
Yosuke Maeda <yosuke_maeda@hotmail.com>
2018-06-20 11:00:55 +0900

    1.0

--- SidifyKludge/SidifyKludge.cs
+++ SidifyKludge/SidifyKludge.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FileFinder;
 
-// Sidifyのバグの後始末をするプログラム（要は、13Jun2018の.wavは削除したい。しかし、問題の無いファイルも混ざっておる。それらの特定が・・・）。
-namespace SidifyKludge {
+namespace FileFinder {
 	class Program {
 		static StreamWriter output;
-		static StreamWriter movers;
 		static bool isMove = false;
 		static bool isOfDate = false;
 		static bool isSidify = false;
 		static string DestFolder;
 		static DirectoryInfo sidifyOut = null;
-		static DateTime ofDate = DateTime.MinValue;
+		static DateTime ofDate=DateTime.MinValue;
 		static void Main(string[] args) {
-			string MyDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			string MyDocuments=Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 			isMove=Environment.CommandLine.Contains("--move");
 			isOfDate = Environment.CommandLine.Contains("--ofdate;");
 			isSidify=Environment.CommandLine.Contains("--sidifykludge;");
@@ -29,6 +26,7 @@ namespace SidifyKludge {
 				DestFolder=String.Format("{0}_{1}",DestFolder,DateTime.Today.ToString("ddMMMyyyy"));
 				Directory.CreateDirectory(DestFolder);
 			}
+			// Sidifyのバグの後始末をするプログラム（要は、13Jun2018の.wavは削除したい）。
 			if(isOfDate) {
 				string[] argus = Environment.GetCommandLineArgs();
 				foreach(string arg in argus) {
@@ -54,6 +52,7 @@ namespace SidifyKludge {
 					}
 				}
 			}
+			//'*' must be the first arg!!
 			for(int i = 0;i<args.Length;++i) {
 				if(args[i].StartsWith("--")) {
 					continue;
@@ -66,32 +65,14 @@ namespace SidifyKludge {
 						outtext.Delete();
 					}
 				}
-				FileInfo moveout = new FileInfo(String.Format("movers_{0}.txt",i));
-				if(moveout.Exists) {
-					if(moveout.Length>0) {
-						WinAPI.MoveToRecycleBin(moveout);
-					} else {
-						moveout.Delete();
-					}
-				}
-				if(isMove) {
-					movers=new StreamWriter(moveout.FullName) { AutoFlush=true };
-				}
 				using(output=new StreamWriter(outtext.FullName) { AutoFlush=true }) {
 					if(isSidify&&sidifyOut!=null) {
-						string pattern = "*.wav";
-						int index = 0;
-						Debug.Assert(String.IsNullOrEmpty(args[index])==false);
-						pattern=args[index];
-						Traverse(sidifyOut,pattern);
+						Traverse(sidifyOut,"*.wav");
 					} else {
 						Traverse(args[i]);
 					}
 					Report("{0}",new String('-',80));
 				}
-			}
-			if(movers!=null) {
-				movers.Close();
 			}
 		}
 		private static void Traverse(string fileSpec) {
@@ -115,7 +96,7 @@ namespace SidifyKludge {
 				if(Contained(folder,fileSpec)) {
 					Report(folder);
 				}
-			} catch { }
+			} catch {}
 			try {
 				DirectoryInfo[] dirs = folder.GetDirectories();
 				foreach(DirectoryInfo dir in dirs) {
@@ -133,16 +114,10 @@ namespace SidifyKludge {
 					foreach(FileInfo file in files) {
 						Traverse(file,fileSpec);
 						if(isMove) {
-							Debug.Assert(movers!=null);
 							if(!AlreadyExists(file,out string newfile)) {
-								if(isOfDate){
-									if(HandleOfDate(file)) {
-										movers.WriteLine("'{0}','{1}'",newfile,file.FullName);
-										file.MoveTo(newfile);
-									}
-								} else {
-									file.MoveTo(newfile);
-								}
+								//元に戻せない由、危険に付き、コメントとした！！
+								//file.MoveTo(newfile);
+								Report("{0}",file.FullName);
 							} else {
 								WinAPI.MoveToRecycleBin(file);
 							}
@@ -165,7 +140,7 @@ namespace SidifyKludge {
 			FileInfo destfile = new FileInfo(Path.Combine(DestFolder,Path.GetFileName(sourfile.FullName)));
 			newfile=destfile.FullName;
 			if(destfile.Exists) {
-				bool isTrue =
+				bool isTrue=
 					destfile.FullName==sourfile.FullName&&
 					destfile.LastAccessTime==sourfile.LastAccessTime&&
 					destfile.Length==sourfile.Length;
@@ -180,32 +155,8 @@ namespace SidifyKludge {
 		}
 
 		private static void Traverse(FileInfo file,string fileSpec) {
-			if(isOfDate) {
-				if(HandleOfDate(file)) {
-					if(SidifyBug(file,out bool showFolder)) {
-						WinAPI.MoveToRecycleBin(file);
-						Report(file,showFolder);
-					}
-				}
-			} else {
-				Report(file);
-			}
+			Report(file);
 		}
-
-		private static bool SidifyBug(FileInfo file,out bool isTrue) {
-			string ext = Path.GetExtension(file.FullName);
-			isTrue=ext.ToLower()=="wav.wav";
-			if(!isTrue) {
-				string[] words = Path.GetFileNameWithoutExtension(file.FullName).Split();
-				if(words.Length==1) {
-					DirectoryInfo folder = new DirectoryInfo(Path.GetDirectoryName(file.FullName));
-					FileInfo[] files = folder.GetFiles(String.Format("{0}*",words[0]));
-					isTrue = files.Length>=2;
-				}
-			}
-			return isTrue;
-		}
-
 		private static void Report(Exception ex) {
 			Console.ForegroundColor=ConsoleColor.Red;
 			Report("{0}",ex.Message);
@@ -224,18 +175,18 @@ namespace SidifyKludge {
 			output.WriteLine(text);
 			Report("{0}",text);
 		}
-		private static void Report(FileInfo file,bool showFolder=false) {
+		private static void Report(FileInfo file) {
 			DateTime from = file.CreationTime;
 			DateTime upto = file.LastWriteTime;
 			if(from>upto) {
 				swap(ref from,ref upto);
 			}
-			string text = String.Empty;
-			if(showFolder) {
-				Uri url = new Uri(Path.GetDirectoryName(file.FullName));
-				string ftex = Uri.UnescapeDataString(url.AbsoluteUri);
-				ftex=Uri.UnescapeDataString(ftex);
-				text = String.Format(@"{0:ddMMMyyyy} | {1:ddMMMyyyy} | {3,15:###,###,###,###,###} | ""{4}"" | {2}",from,upto,file.FullName,file.Length,ftex);
+			string text = null;
+			if(isOfDate) {
+				bool ofdate = HandleOfDate(file);
+				if(ofdate) {
+					text = String.Format("{0:ddMMMyyyy} | {1:ddMMMyyyy} | {3,15:###,###,###,###,###} | {2}",from,upto,file.FullName,file.Length);
+				}
 			} else {
 				text = String.Format("{0:ddMMMyyyy} | {1:ddMMMyyyy} | {3,15:###,###,###,###,###} | {2}",from,upto,file.FullName,file.Length);
 			}
